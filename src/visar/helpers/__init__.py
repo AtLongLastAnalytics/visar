afc2d4023c0ae4a6cb17782c59c09b99bbf527b0
Rob Long <rob@atlonglastanalytics.com>
2026-06-27 13:09:32 -0600

    code cleanup and refactoring of the dashboard code, removing unused files and updating the README to reflect the changes.

--- src/visar/helpers/__init__.py
+++ src/visar/helpers/__init__.py
@@ -6,7 +6,7 @@ Licensed under the Apache License, Version 2.0
 Project: https://github.com/AtLongLastAnalytics/visar
 Author: Robert Long
 Date: 2026-03
-Version: 1.1.0
+Version: 1.2.0
 
 File: __init__.py
 Description: This package contains utility modules for:
